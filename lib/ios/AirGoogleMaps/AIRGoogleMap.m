ee00ae2c4945d3f1b4eae7aba667599ded100f77
William Murphy <willardtwashington@gmail.com>
2022-04-20 13:26:53 -0500

    no message

--- lib/ios/AirGoogleMaps/AIRGoogleMap.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMap.m
@@ -150,8 +150,12 @@ id regionAsJSON(MKCoordinateRegion region) {
     [self.circles addObject:circle];
   } else if ([subview isKindOfClass:[AIRGoogleMapUrlTile class]]) {
     AIRGoogleMapUrlTile *tile = (AIRGoogleMapUrlTile*)subview;
-    tile.tileLayer.map = self;
-    [self.tiles addObject:tile];
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      tile.tileLayer.map = self;
+      [self.tiles addObject:tile];
+    });
   } else if ([subview isKindOfClass:[AIRGoogleMapWMSTile class]]) {
     AIRGoogleMapWMSTile *tile = (AIRGoogleMapWMSTile*)subview;
     tile.tileLayer.map = self;
